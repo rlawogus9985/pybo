ee67c862d3ff897de48ad9b5272161b6f2d6c9c5
김재현 <rlawogus9985@naver.com>
2022-05-11 19:13:10 +0900

    logging 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -133,7 +133,7 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'filters': {
-        'require_debug.false': {
+        'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse',
         },
         'require_debug_true': {
