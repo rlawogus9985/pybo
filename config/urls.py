c33bc26b5a4bf7a7d1923a2ac3ba084ff3c9f6e7
김재현 <rlawogus9985@naver.com>
2022-05-25 18:44:48 +0900

    image path, random accuracy 05251844

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from pybo.views import base_views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,5 @@ urlpatterns = [
 ]
 
 handler404 = 'common.views.page_not_found'
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
